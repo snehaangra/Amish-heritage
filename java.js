6fd3e32a33997423b44edc94465bbdbf8f78964e
Sneha Angra <angra.sneha@gmail.com>
2026-05-12 21:38:31 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,1 @@
-const btn = document.querySelector('#myButton');
 
-btn.addEventListener('click', () => {
-    alert('Button was clicked!');
-    console.log('The user interacted with the button.');
-});
