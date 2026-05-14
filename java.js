e539ff72cf73bb16c8a3793910dae770ad52ef9e
BeaverKing888 <alex@xandermartin.com>
2026-05-13 20:32:14 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,14 +1,12 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
-    const fileUrl = 'path/to/your/file.zip';
-    const fileName = 'MyPackage.zip';
+    const fileUrl = '4800_001.pdf'; 
+    const fileName = 'Student_Copyright_Checklist.pdf';
 
     const link = document.createElement('a');
     link.href = fileUrl;
     link.download = fileName;
 
     document.body.appendChild(link);
-
     link.click();
-
     document.body.removeChild(link);
 });
