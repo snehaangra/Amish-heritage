f337e92a75ebcb924e3a76923bcfdaebf36d1b81
BeaverKing888 <alex@xandermartin.com>
2026-05-13 20:56:51 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,12 +1,16 @@
-document.getElementById('downloadBtn').addEventListener('click', function() {
-    const fileUrl = '4800_001.pdf'; 
-    const fileName = 'Student_Copyright_Checklist.pdf';
-
+function startDownload(fileUrl, fileName) {
     const link = document.createElement('a');
     link.href = fileUrl;
     link.download = fileName;
-
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+}
+
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    startDownload('4800_001.pdf', 'Student_Copyright_Checklist.pdf');
+});
+
+document.getElementById('logBtn').addEventListener('click', function() {
+    startDownload('4802_001.pdf', 'TSA_Work_Log.pdf');
 });
